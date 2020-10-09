92541a5c9f7401ab1764c45bd3ff5f7883027fb8
aldrinburgos <burgos.aldrin@gmail.com>
2020-10-09 14:55:51 -0500

    fighting through each robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -13,10 +13,12 @@ var enemyAttack = 12;
 //  *Defeat each enemy robot
 //"LOSE" - Player robot's health is zero or less
 
-var fight = function (enemyName) {
+
   //Alert users that they are starting the round.
   window.alert("Welcome to Robot Gladiators!");
-
+  var fight = function () {
+  while(enemyHealth > 0) {
+  
   var promptFight = window.prompt(
     "Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
   );
@@ -81,8 +83,12 @@ var fight = function (enemyName) {
   } else {
     window.alert("You need to pick a valid option. Try again!");
   }
+}
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-  fight(enemyNames[i]);
+  var pickedEnemyName = enemyNames[i];
+  enemyHealth = 50;
+  //call fight function with enemy robot
+  fight(pickedEnemyName);
 }
