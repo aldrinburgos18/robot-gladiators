6cfadf19f09b3c2e31a20ca17a032a54e6f2b932
aldrinburgos <burgos.aldrin@gmail.com>
2020-10-09 19:21:26 -0500

    random health and attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,10 @@
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
-var playerAttack = 50;
+var playerAttack = 10;
 var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
+var enemyHealth = enemyHealth = Math.floor(Math.random() * 21) + 40;
 var enemyAttack = 12;
 
 //Game States
@@ -22,8 +22,9 @@ var enemyAttack = 12;
       // ask user if they'd liked to fight or run
       var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
       if (promptFight === "fight" || promptFight === "FIGHT") {
-            // remove enemy's health by subtracting the amount set in the playerAttack variable
-            enemyHealth = enemyHealth - playerAttack;
+            // generate random damage value based on player's attack power
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(
               playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.'
             );
@@ -42,7 +43,8 @@ var enemyAttack = 12;
             }
         
             // remove players's health by subtracting the amount set in the enemyAttack variable
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(
               enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.'
             );
@@ -66,7 +68,7 @@ var enemyAttack = 12;
         if (confirmSkip) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
-          playerMoney = playerMoney - 10;
+          playerMoney = Math.max(0, playerMoney - 10);
           console.log("playerMoney", playerMoney)
           break;
         }
@@ -83,8 +85,10 @@ var startGame = function() {
 
   //reset player stats
   playerHealth = 100;
-  playerAttack = 50;
+  playerAttack = 10;
   playerMoney = 10;
+
+ 
 
 for (var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
@@ -95,7 +99,7 @@ for (var i = 0; i < enemyNames.length; i++) {
     var pickedEnemyName = enemyNames[i];
 
     // reset enemyHealth before starting new fight
-    enemyHealth = 50;
+    enemyHealth = randomNumber(40, 60);
 
     // use debugger to pause script from running and check what's going on at that moment in the code
     // debugger;
@@ -187,6 +191,13 @@ switch (shopOptionPrompt) {
 }
 };
   
+// function to generate a random numeric value
+var randomNumber = function(min, max) {
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+  return value;
+};
+
 
 //start the game when the page loads
 startGame();
